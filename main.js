b68a6b886ced3d6046362afc35b36ef00fd491f5
saisriharshith <konthamsaisriharshith@gmail.com>
2026-05-25 21:17:34 +0530

    portfolio

---
+++ main.js
@@ -0,0 +1,12 @@
+var typed = new Typed(".text", {
+    strings: [
+        "AI & Data Science Student",
+        "Frontend Developer",
+        "Web Designer",
+        "Tech Enthusiast"
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
