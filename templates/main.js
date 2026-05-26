c6180d8ae3acef4a68cd3e71edec965d89a54ef9
saisriharshith <konthamsaisriharshith@gmail.com>
2026-05-26 12:05:01 +0530

    yes

---
+++ templates/main.js
@@ -0,0 +1,15 @@
+var typed = new Typed(".text", {
+    strings: [
+        "Viswam AI Internship Team",
+        "AI & Data Science Learners",
+        "Frontend Developers",
+        "Future AI Innovators",
+        "Tech Enthusiasts",
+        "Creative Problem Solvers",
+        "Learning AI, Web & Innovation"
+    ],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
